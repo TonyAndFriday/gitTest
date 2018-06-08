1dba077c43fdffe228cbe8ddb431692c99429c16
panbinbin <panbb@trustfar.cn>
2018-06-08 16:24:35 +0800

    Rejected commit: Default

---
+++ src/tony/test/Test.java
@@ -0,0 +1,10 @@
+package tony.test;
+
+/**
+ * Created by tony on 2018/4/28.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
